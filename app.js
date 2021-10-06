9a8565e4a662e93965650a74a13daf07b78bc3ba
Daniel Guzman <42854222+dannyguzman31@users.noreply.github.com>
2021-10-06 17:08:47 -0600

    Countries API

---
+++ app.js
@@ -0,0 +1,135 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const e = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+// 
+app.use(express.static("public"));
+// local connection to localhost db
+mongoose.connect("mongodb://localhost:27017/countriesDB", { useNewUrlParser: true });
+// country schema object 
+const countrySchema = {
+  country: String,
+  capital: String,
+  continent: String
+};
+
+const Country = mongoose.model("Countries", countrySchema);
+// Request Targeting all specific articles
+app
+  .route("/countries") // get data from db 
+  .get(function (req, res) {
+    Country.find(function (err, foundCountries) {
+      if (!err) {
+        res.send(foundCountries);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+// Add new data (country) to database
+  .post(function (req, res) {
+    const newCountry = new Country({
+      country: req.body.country,
+      capital: req.body.capital,
+      continent: req.body.continent,
+    });
+    newCountry.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new country.");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+// deletes all countries (data) from db 
+  .delete(function(req, res){
+    Country.deleteMany(function (err){
+      if(!err){
+        res.send("Successfully deleted all countries.");
+      }
+      else{
+        res.send(err);
+      }
+    })
+  });
+
+// 
+
+/************* REQUEST BY SPECIFIC TARGET COUNTRY *******/
+
+app.
+route("/countries/:countryTitle")
+// find a country by name
+.get(function (req, res){
+  Country.findOne(
+    {country: req.params.countryTitle},
+    function (err, foundCountry) {
+      if(foundCountry){
+        res.send(foundCountry);
+      }
+      else {
+        res.send("No countries matching found!");
+      }
+    }
+  )
+})
+// update a country data 
+.put(function(req, res){
+  Country.replaceOne(
+    {country: req.params.countryTitle},
+    {country: req.body.country, capital: req.body.capital, continent: req.body.continent},
+    {overwrite: true},
+    function (err) {
+      if(!err){
+        res.send("Successfully updated country");
+      }
+      else {
+        res.send(err);
+      }
+    }
+  )
+})
+
+.patch(function (req, res) {
+  Country.updateOne(
+    { country: req.params.countryTitle },
+    { $set: req.body },
+    function (err) {
+      if (!err) {
+        res.send("Successfully updated country.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+.delete(function (req, res) {
+  Country.deleteOne({ country: req.params.countryTitle }, function (err) {
+    if (!err) {
+      res.send("Successfully deleted country.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+
+
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
---
+++ database/africa.sql
@@ -0,0 +1,369 @@
+/*
+ -- Africa database: 
+*/
+
+{
+    "_id" : ObjectId("615dc932c39045504a69caff"),
+    "country" : "Algeria",
+    "capital" : "Algiers",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbdb"),
+    "country" : "Angola",
+    "capital" : "Luanda",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbde"),
+    "country" : "Benin",
+    "capital" : "Porto-Novo",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbe1"),
+    "country" : "Botswana",
+    "capital" : "Gaborone",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbe4"),
+    "country" : "Burkina Faso",
+    "capital" : "Ouagadougou",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbe7"),
+    "country" : "Burundi",
+    "capital" : "Bujumbura",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbea"),
+    "country" : "Cameroon",
+    "capital" : "Yaounde",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbed"),
+    "country" : "Cape Verde",
+    "capital" : "Praia",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbf0"),
+    "country" : "Central African Republic",
+    "capital" : "Bangui",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbf3"),
+    "country" : "Chad",
+    "capital" : "N'Djamena",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbf6"),
+    "country" : "Comoros",
+    "capital" : "Moroni",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbf9"),
+    "country" : "Congo",
+    "capital" : "Democratic Republic of the Kinshasa",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbfc"),
+    "country" : "Congo",
+    "capital" : "Republic of the, Brazzaville",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cbff"),
+    "country" : "Cote d'Ivoire",
+    "capital" : "Yamoussoukro",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc02"),
+    "country" : "Djibouti",
+    "capital" : "Djibouti",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc05"),
+    "country" : "Egypt",
+    "capital" : "Cairo",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc08"),
+    "country" : "Equatorial Guinea",
+    "capital" : "Malabo",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc0b"),
+    "country" : "Eritrea",
+    "capital" : "Asmara",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc0e"),
+    "country" : "Eswatini",
+    "capital" : "Mbabane, Lobamba",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc11"),
+    "country" : "Ethiopia",
+    "capital" : "Addis Ababa",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc14"),
+    "country" : "Gabon",
+    "capital" : "Libreville",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc17"),
+    "country" : "Gambia",
+    "capital" : "The Banjul",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc1a"),
+    "country" : "Ghana",
+    "capital" : "Accra",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc1d"),
+    "country" : "Guinea",
+    "capital" : "Conakry",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc20"),
+    "country" : "Guinea-Bissau",
+    "capital" : "Bissau",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc23"),
+    "country" : "Kenya",
+    "capital" : "Nairobi",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc26"),
+    "country" : "Lesotho",
+    "capital" : "Maseru",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc29"),
+    "country" : "Liberia",
+    "capital" : "Monrovia",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc2c"),
+    "country" : "Libya",
+    "capital" : "Tripoli",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc2f"),
+    "country" : "Madagascar",
+    "capital" : "Antananarivo",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc32"),
+    "country" : "Malawi",
+    "capital" : "Lilongwe",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc35"),
+    "country" : "Mali",
+    "capital" : "Bamako",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc38"),
+    "country" : "Mauritania",
+    "capital" : "Nouakchott",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc3b"),
+    "country" : "Mauritius",
+    "capital" : "Port Louis",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc3e"),
+    "country" : "Morocco",
+    "capital" : "Rabat",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc41"),
+    "country" : "Mozambique",
+    "capital" : "Maputo",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc44"),
+    "country" : "Namibia",
+    "capital" : "Windhoek",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc47"),
+    "country" : "Niger",
+    "capital" : "Niamey",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc4a"),
+    "country" : "Nigeria",
+    "capital" : "Abuja",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc4d"),
+    "country" : "Rwanda",
+    "capital" : "Kigali",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc50"),
+    "country" : "Sao Tome and Principe",
+    "capital" : "Sao Tome",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc53"),
+    "country" : "Senegal",
+    "capital" : "Dakar",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc56"),
+    "country" : "Seychelles",
+    "capital" : "Victoria",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc59"),
+    "country" : "Sierra Leone",
+    "capital" : "Freetown",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc5c"),
+    "country" : "Somalia",
+    "capital" : "Mogadishu",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc5f"),
+    "country" : "South Africa, Pretoria",
+    "capital" : "Cape Town",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc62"),
+    "country" : "Sudan",
+    "capital" : "Khartoum",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc65"),
+    "country" : "Tanzania",
+    "capital" : "Dar es Salaam",
+    "continent": "Africa"
+}
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc68"),
+    "country" : "Togo",
+    "capital" : "Lome",
+    "continent": "Africa"
+}
+
+
+{
+    "_id" : ObjectId("615dd807c39045504a69cc6b"),
+    "country" : "Tunisia",
+    "capital" : "Tunis",
+    "continent": "Africa"
+}
